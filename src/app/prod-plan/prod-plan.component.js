943f2a21916dc9a645e94cc7f74a4083687e4612
francis garcia <francis.g@electronicscience.com>
2019-01-01 19:41:49 +0800

    added dropwon options

--- src/app/prod-plan/prod-plan.component.js
+++ src/app/prod-plan/prod-plan.component.js
@@ -3,6 +3,9 @@ angular.module("ProdPlanModule")
     var vm = this; //ppc
 
     vm.mainData = [];
+    vm.cropFamily = [];
+    vm.cropName = [];
+    vm.cropVariety = [];
     vm.minDate = new Date(Date.now());
 
     vm.datatable = tableLimitAndPage();
@@ -18,12 +21,34 @@ angular.module("ProdPlanModule")
           vm.loadingData = false;
           
           //push object list to array for repeat in html
-          let keys = Object.keys(success.ProductionPlanning);;
-          keys.forEach(key => {
+          let prodPlanKeys = Object.keys(success.ProductionPlanning);
+          prodPlanKeys.forEach(key => {
             vm.mainData.push(success.ProductionPlanning[key]);
           })
 
-          console.log(success);
+          //get produce data for dorpdowns
+          let produceKeys = Object.keys(success.Produce);
+          produceKeys.forEach(key => {
+            if(success.Produce[key].CropFamily) {
+              vm.cropFamily.push(success.Produce[key].CropFamily);
+            }
+
+            if(success.Produce[key].CropName) {
+              vm.cropName.push(success.Produce[key].CropName);
+            }
+
+            if(success.Produce[key].Variety) {
+              let varietyKeys = Object.keys(success.Produce[key].Variety);
+              varietyKeys.forEach(variety => {
+                vm.cropVariety.push(variety);
+              })              
+            }
+          })
+
+          vm.cropFamily = Array.from(new Set(vm.cropFamily));
+          vm.cropName = Array.from(new Set(vm.cropName));
+          vm.cropVariety = Array.from(new Set(vm.cropVariety));
+          console.log(vm.cropFamily, vm.cropName, vm.cropVariety);
         })
         .catch(function (error) {
           console.error(error);
